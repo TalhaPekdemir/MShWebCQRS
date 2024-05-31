3d2882a5eccc1a430005c30a22c2080d98a1bade
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2024-05-31 13:30:25 +0300

    crlf fixes, removal of unnecessary lines

--- src/MShWeb/MShWeb.Application/Services/Images/ImageManager.cs
+++ src/MShWeb/MShWeb.Application/Services/Images/ImageManager.cs
@@ -3,7 +3,6 @@ using MShWeb.Application.Services.Repositories;
 using MShWeb.Domain.Entities;
 using System.Linq.Expressions;
 using MShWeb.Application.Services.Files;
-using Microsoft.Extensions.FileProviders;
 using AutoMapper;
 
 
@@ -66,61 +65,5 @@ namespace MShWeb.Application.Services.Images
         {
             throw new NotImplementedException();
         }
-
-        
-
-
-
-        //private async Task<Image> SaveFileAsync(IFormFile file)
-        //{
-        //    // requirements: path, filename, extension
-        //    // don't trust filename genrate guid
-        //    // dont trust extension, can be different but can't be bothered to check right now
-        //    // file.ContentType maybe???
-        //    // get path somehow better
-
-        //    string path = Path.Combine(HttpContextHelper.Env.WebRootPath, "uploads");
-        //    string fileName = Guid.NewGuid().ToString();
-        //    string extension = Path.GetExtension(file.FileName);
-
-        //    EnsureDirectoryExists(path);
-
-        //    string pathToSave = Path.Combine(path, fileName + extension);
-        //    using (FileStream stream = File.Create(pathToSave))
-        //    {
-        //        await file.CopyToAsync(stream);
-        //    }
-
-        //    return new Image()
-        //    {
-        //        CreatedDate = DateTime.UtcNow,
-        //        Source = fileName + extension,
-        //    };
-        //}
-
-
-        //private async Task<List<Image>> SaveFilesAsync(List<IFormFile> files)
-        //{
-        //    List<Image> images = new List<Image>();
-
-        //    foreach (var file in files)
-        //    {
-        //        if (file.Length > 0)
-        //        {
-        //            Image image = await SaveFileAsync(file);
-        //            images.Add(image);
-        //        }
-        //    }
-
-        //    return images;
-        //}
-
-        //private void EnsureDirectoryExists(string filePath)
-        //{
-        //    if (!Directory.Exists(filePath))
-        //    {
-        //        Directory.CreateDirectory(filePath);
-        //    }
-        //}
     }
 }
